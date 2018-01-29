8e0dbfeccc161187b6a4d58ff5bfe5c29301e395
Nitzan Frock <nitzanfrock@gmail.com>
2018-01-29 11:24:28 -0500

    eject screwed up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -47,13 +47,6 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
 
     let persons = null;
 
@@ -72,12 +65,19 @@ class App extends Component {
       );
     }
 
+    const assignedClasses = [];
+    if (this.state.persons.length <=2){
+      assignedClasses.push(classes.red);
+    }
+    if (this.state.persons.length <=1){
+      assignedClasses.push(classes.bold);
+    }
+
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>This is a React App</h1>
-        <p>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
-          style={style}
           onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
       </div>
