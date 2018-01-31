01fa7dcb23171bd4653a97ed1a7f812fd5a630f8
Nitzan Frock <nitzanfrock@gmail.com>
2018-01-30 19:57:05 -0500

    validating props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
 
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
@@ -20,5 +21,12 @@ class Person extends Component {
         // ]
     }
 }
+
+Person.PropTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
